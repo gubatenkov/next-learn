2adc63562fad11a10f94c9481f35113414830c69
V <v@macbook.local>
2023-11-21 16:44:14 +0200

    .

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,7 +9,7 @@ export const metadata: Metadata = {
     default: 'Freedom Dashboard',
   },
   description: 'The official Next.js Learn Dashboard built with App Router.',
-  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+  metadataBase: new URL('https://next-learn-dash.vercel.app'),
 };
 
 export default function RootLayout({
